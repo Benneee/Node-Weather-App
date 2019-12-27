265be8743a8effd6a7b677ecd59282ca564dc5cb
benneee <benedictiknkeonye@gmail.com>
2019-12-27 23:53:02 +0100

    Customised the request responses

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 const request = require("request");
+const log = console.log;
+
+const url =
+  "https://api.darksky.net/forecast/0bb3f9a06986871e44f977a63529a356/37.8267,-122.4233?units=si";
+
+request({ url: url, json: true }, (error, response) => {
+  //   log(response.body.currently); // The current data report of the said location
+  const data = response.body.currently;
+  const dailyData = response.body.daily.data[0];
+  log(
+    `${dailyData.summary} It is currently ${data.temperature} degrees out. There is a ${data.precipProbability}% chance of rain`
+  );
+});
