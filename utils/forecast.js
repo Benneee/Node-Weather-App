896805770224e1d6ebb2e5cd7f5b6281a505eacd
benneee <benedictiknkeonye@gmail.com>
2019-12-28 09:49:42 +0100

    Now receiving users' input for weather forecat

--- utils/forecast.js
+++ utils/forecast.js
@@ -1,32 +1,5 @@
 const request = require("request");
 
-// Goal: Create a reusable function for getting the forecast
-//
-// 1. Setup the "forecast" function in utils/forecast.js
-// 2. Require the function in app.js and call it as shown below
-// 3. The forecast function should have three potential calls to callback:
-//    - Low level error, pass string for error
-//    - Coordinate error, pass string for error
-//    - Success, pass forecast string for data (same format as from before)
-
-// const url =
-//   "https://api.darksky.net/forecast/0bb3f9a06986871e44f977a63529a356/37.8267,-122.4233?units=si";
-
-// request({ url: url, json: true }, (error, response) => {
-//   if (error) {
-//     log(`Unable to connect to weather service`);
-//   } else if (response.body.error) {
-//     log("Unable to find location");
-//   } else {
-//     //   log(response.body.currently); // The current data report of the said location
-//     const data = response.body.currently;
-//     const dailyData = response.body.daily.data[0];
-//     log(
-//       `${dailyData.summary} It is currently ${data.temperature} degrees out. There is a ${data.precipProbability}% chance of rain`
-//     );
-//   }
-// });
-
 const forecast = (longitude, latitude, callback) => {
   const url = `https://api.darksky.net/forecast/0bb3f9a06986871e44f977a63529a356/${latitude},${longitude}?units=si`;
 
